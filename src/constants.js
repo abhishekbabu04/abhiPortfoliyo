8593c12309bc6e327fcfff6bceb9244ee7abb570
abhishek <babuabhishek920@gmail.com>
2026-06-15 11:32:33 +0530

    first commit

---
+++ src/constants.js
@@ -0,0 +1,226 @@
+// Skills Section Logo's
+import htmlLogo from './assets/tech_logo/html.png';
+import cssLogo from './assets/tech_logo/css.png';
+// import sassLogo from './assets/tech_logo/sass.png';
+import javascriptLogo from './assets/tech_logo/javascript.png';
+import reactjsLogo from './assets/tech_logo/reactjs.png';
+// import angularLogo from './assets/tech_logo/angular.png';
+// import reduxLogo from './assets/tech_logo/redux.png';
+// import nextjsLogo from './assets/tech_logo/nextjs.png';
+import tailwindcssLogo from './assets/tech_logo/tailwindcss.png';
+// import gsapLogo from './assets/tech_logo/gsap.png';
+// import materialuiLogo from './assets/tech_logo/materialui.png';
+import bootstrapLogo from './assets/tech_logo/bootstrap.png';
+// import springbootLogo from './assets/tech_logo/springboot.png';
+import nodejsLogo from './assets/tech_logo/nodejs.png';
+import expressjsLogo from './assets/tech_logo/express.png';
+import mysqlLogo from './assets/tech_logo/mysql.png';
+import mongodbLogo from './assets/tech_logo/mongodb.png';
+// import firebaseLogo from './assets/tech_logo/firebase.png';
+import cLogo from './assets/tech_logo/c.png';
+// import cppLogo from './assets/tech_logo/cpp.png';
+import javaLogo from './assets/tech_logo/java.png';
+import pythonLogo from './assets/tech_logo/python.png';
+// import typescriptLogo from './assets/tech_logo/typescript.png';
+import gitLogo from './assets/tech_logo/git.png';
+import githubLogo from './assets/tech_logo/github.png';
+import vscodeLogo from './assets/tech_logo/vscode.png';
+// import postmanLogo from './assets/tech_logo/postman.png';
+// import mcLogo from './assets/tech_logo/mc.png';
+// import figmaLogo from './assets/tech_logo/figma.png';
+import netlifyLogo from './assets/tech_logo/netlify.png';
+// import vercelLogo from './assets/tech_logo/vercel.png';
+// import postgreLogo from './assets/tech_logo/postgre.png';
+// import csharpLogo from './assets/tech_logo/csharp.png';
+
+// Experience Section Logo's
+import rprdLogo from './assets/company_logo/rprdLogo.webp';
+// import agcLogo from './assets/company_logo/agc_logo.png';
+// import newtonschoolLogo from './assets/company_logo/newtonschool_logo.png';
+
+ // Education Section Logo's
+ import csjmaLogo from './assets/education_logo/csjmaLogo.png';
+ import collegeLogo from './assets/education_logo/collegeLogo.jpg';
+// import vpsLogo from './assets/education_logo/vps_logo.png';
+
+// Project Section Logo's
+import bootsProject from './assets/work_logo/boots.png';
+import jsuiLogo from './assets/work_logo/jsuiLogo.png';
+import movierecLogo from './assets/work_logo/movie_rec.png';
+import taskremLogo from './assets/work_logo/task_rem.png';
+import npmLogo from './assets/work_logo/npm.png';
+import webverLogo from './assets/work_logo/web_dig.png';
+import cmLogo from './assets/work_logo/cm.png';
+import imagesearchLogo from './assets/work_logo/image_search.png';
+import removebgLogo from './assets/work_logo/remove_bg.png';
+
+
+export const SkillsInfo = [
+  {
+    title: 'Frontend',
+    skills: [
+      { name: 'HTML', logo: htmlLogo },
+      { name: 'CSS', logo: cssLogo },
+      // { name: 'SASS', logo: sassLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      { name: 'React JS', logo: reactjsLogo },
+      // { name: 'Angular', logo: angularLogo },
+      // { name: 'Redux', logo: reduxLogo },
+      // { name: 'Next JS', logo: nextjsLogo },
+      { name: 'Tailwind CSS', logo: tailwindcssLogo },
+      // { name: 'GSAP', logo: gsapLogo },
+      // { name: 'Material UI', logo: materialuiLogo },
+      { name: 'Bootstrap', logo: bootstrapLogo },
+    ],
+  },
+  {
+    title: 'Backend',
+    skills: [
+      // { name: 'Springboot', logo: springbootLogo },
+      { name: 'Node JS', logo: nodejsLogo },
+      { name: 'Express JS', logo: expressjsLogo },
+      { name: 'MySQL', logo: mysqlLogo },
+      { name: 'MongoDB', logo: mongodbLogo },
+      // { name: 'Firebase', logo: firebaseLogo },
+      // { name: 'PostgreSQL', logo: postgreLogo },
+    ],
+  },
+  {
+    title: 'Languages',
+    skills: [
+      { name: 'C', logo: cLogo },
+      // { name: 'C++', logo: cppLogo },
+      { name: 'Java', logo: javaLogo },
+      { name: 'Python', logo: pythonLogo },
+      // { name: 'C-Sharp', logo: csharpLogo },
+      { name: 'JavaScript', logo: javascriptLogo },
+      // { name: 'TypeScript', logo: typescriptLogo },
+    ],
+  },
+  {
+    title: 'Tools',
+    skills: [
+      { name: 'Git', logo: gitLogo },
+      { name: 'GitHub', logo: githubLogo },
+      { name: 'VS Code', logo: vscodeLogo },
+      // { name: 'Postman', logo: postmanLogo },
+      // { name: 'Compass', logo: mcLogo },
+      // { name: 'Vercel', logo: vercelLogo },
+      { name: 'Netlify', logo: netlifyLogo },
+      // { name: 'Figma', logo: figmaLogo },
+    ],
+  },
+];
+
+  export const experiences = [
+    {
+      id: 0,
+      img: rprdLogo,
+      role: "Fullstack Developer",
+      company: "RPRD Group Company",
+      date: "July 2026 - Present",
+      desc: "Developed dynamic and scalable web applications using the MERN stack, handling both frontend and backend development. Collaborated with cross-functional teams to build responsive UI, implement RESTful APIs, and optimize application performance in an agile environment.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "bootstrap",
+        "React JS",
+        "Node JS",
+        "Express JS",
+        "Tailwind CSS",
+        "MongoDb",
+        
+      ],
+    },
+    // {
+    //   id: 1,
+    //   img: agcLogo,
+    //   role: "Fullstack Engineer",
+    //   company: "Agumentik Group of Companies",
+    //   date: "July 2023 - March 2024",
+    //   desc: "Contributed to innovative projects as a Fullstack Engineer, leading both frontend and backend development using technologies such as HTML, CSS, JavaScript, PHP, SQL, Bootstrap, and ReactJS. Worked closely with the team to deliver responsive, high-performance web applications and improve user experience through seamless integration of various technologies.",
+    //   skills: [
+    //     "ReactJS",
+    //     "Redux",
+    //     "JavaScript",
+    //     "Tailwind CSS",
+    //     "HTML",
+    //     "CSS",
+    //     "SQL",
+    //   ],
+    // },
+    {
+      id: 1,
+      img: rprdLogo,
+      role: "Frontend Intern",
+      company: "RPRD Group Company",
+      date: "2026",
+      desc: "Worked as a Frontend Developer Intern, designing and implementing scalable UI components and responsive websites using HTML, CSS, JavaScript, Bootstrap, and Material UI. Collaborated with the design team to translate wireframes and prototypes from Figma into interactive, user-friendly web pages.",
+      skills: [
+        "HTML",
+        "CSS",
+        "JavaScript",
+        "bootstrap",
+        "React JS",
+        "Node JS",
+        "Express JS",
+        "Tailwind CSS",
+        "MongoDb",
+      ],
+    },
+  ];
+  
+  export const education = [
+    {
+      id: 0,
+      img: csjmaLogo,
+      school: "CSJMU Kanpur",
+      date: "july 2023 - July 2027",
+      grade: "7.81 CGPA",
+      desc: "I have completed my Bachelor's degree (B.Tech) in Computer Science and Engineering from Chhatrapati Shahu Ji Maharaj University (CSJMU), Kanpur. During my academic journey, I built a strong foundation in computer science, software development, and problem-solving. I studied subjects such as Data Structures and Algorithms, Object-Oriented Programming, Database Management Systems, Operating Systems, Computer Networks, Web Development, and Software Engineering. I also worked on academic projects and participated in technical activities that helped enhance my practical knowledge and technical skills. My education at CSJMU has played a significant role in developing my analytical thinking, programming expertise, and professional growth.",
+      degree: "Bachelor of Technology (B.Tech) in Computer Science and Engineering",
+    },
+    {
+      id: 1,
+      img: collegeLogo,
+      school: "Pt. Ichchharam Inter College Rura Kanpur Dehat UP.",
+      date: "2021 - 2022",
+      grade: "70%",
+      desc: "I did my class 12 education from Pt. Ichchharam Inter College Rura Kanpur Dehat, under the UP board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "UP(XII) - PCM with Computer Science",
+    },
+   {
+      id: 2,
+      img: collegeLogo,
+      school: "Pt. Ichchharam Inter College Rura Kanpur Dehat UP.",
+      date: "2019 - 2020",
+      grade: "82%",
+      desc: "I did my class 10 education from Pt. Ichchharam Inter College Rura Kanpur Dehat, under the UP board, where I studied Physics, Chemistry, and Mathematics (PCM) with Computer Science.",
+      degree: "UP(X) - PCM with Computer Science",
+    },
+   ];
+  
+  export const projects = [
+    {
+      id: 0,
+      title: "Bootstrap Layout",
+      description:
+        "A user-friendly bootstrap layout designed . Simply enter a GitHub username, and the app fetches comprehensive data, including profile stats, repositories, followers, and contributions. The intuitive interface ensures a seamless experience, making it a must-visit tool for developers and recruiters.",
+      image: bootsProject,
+      tags: ["HTML", "CSS", "React JS","bootstrap"],
+      github: "https://github.com/abhishekbabu04/wscube-tech-bootstrap",
+      webapp: "https://abhiwscubetech.netlify.app/",
+    },
+    {
+      id: 1,
+      title: "UI js programs",
+      description:
+        "I have created a ui based mini project in which you can measure your BMI,Age calculater,converters like Celsius to Fahrenheit,Kilometers to Miles Converter",
+      image: jsuiLogo,
+      tags: ["HTML", "CSS", "JavaScript"],
+      github: "https://github.com/abhishekbabu04/js-ui-utility-projects",
+      webapp: "https://abhiuijsprograms.netlify.app/",
+    },
+     
+  ];  
